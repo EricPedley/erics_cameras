9de069098aab95e389c3e286974811ac3a3743fc
Eric Pedley <ericpedley@gmail.com>
2025-08-03 21:54:39 -0700

    initial commit
    code looks fine but config is fucked so cant import the package

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+from .camera import Camera as Camera, ImageMetadata as ImageMetadata
+from .gazebo import (
+    GazeboCamera as GazeboCamera,
+)
+from .mock_camera import MockCamera as MockCamera
+from .csi_cam import CSICam as CSICam
+from .usb_cam import USBCam as USBCam
