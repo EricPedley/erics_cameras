a3e78b8d0e532ba33df9a60bd94243cfd059bd62
Eric Pedley <ericpedley@gmail.com>
2025-08-05 15:19:35 -0700

    add option to flip for rtsp camera

--- src/erics_cameras/rtsp_cam.py
+++ src/erics_cameras/rtsp_cam.py
@@ -8,9 +8,11 @@ class RTSPCamera(GstCamera):
         self,
         log_dir: str | Path | None = None,
         url_str: str = "192.168.42.1/h264",
+        flip=False,
     ):
         """
         TODO: make resolution choosable. Hard-coded in JetsonCamera rn.
         """
-        pipeline = f'rtspsrc location=rtsp://{url_str} latency=0 drop-on-latency=true ! rtph264depay ! h264parse ! avdec_h264 max-threads=1 ! videoconvert ! video/x-raw, format=BGRx ! appsink drop=true'
+        flip_str = '! videoflip method=rotate-180' if flip else ''
+        pipeline = f'rtspsrc location=rtsp://{url_str} latency=0 drop-on-latency=true ! rtph264depay ! h264parse ! avdec_h264 max-threads=1 ! videoconvert {flip_str} ! video/x-raw, format=BGRx ! appsink drop=true'
         super().__init__(log_dir, pipeline)
