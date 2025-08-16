0aa3de0e82fbf76a1544d929f9803d797778ba5d
Eric Pedley <ericpedley@gmail.com>
2025-08-15 18:35:59 -0700

    add gstcam to init

--- src/erics_cameras/__init__.py
+++ src/erics_cameras/__init__.py
@@ -3,3 +3,4 @@ from .mock_cam import MockCamera as MockCamera
 from .csi_cam import CSICam as CSICam
 from .usb_cam import USBCam as USBCam
 from .rtsp_cam import RTSPCamera as RTSPCamera
+from .gst_cam import GstCamera
