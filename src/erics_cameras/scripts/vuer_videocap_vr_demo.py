ed4e73057bf220bdf4be12459f1a001dd1f8f422
Eric Pedley <ericpedley@gmail.com>
2025-08-19 12:18:36 -0700

    make vuer thingy use same cam source as calibration

--- src/erics_cameras/scripts/vuer_videocap_vr_demo.py
+++ src/erics_cameras/scripts/vuer_videocap_vr_demo.py
@@ -3,6 +3,7 @@ from vuer import Vuer, VuerSession
 import asyncio
 from vuer.schemas import ImageBackground
 import numpy as np
+from erics_cameras.libcamera_cam import LibCameraCam
 
 cam_mat = np.array([[266.61728276,0.,643.83126137],[0.,266.94450686,494.81811813],[0.,0.,1.,]])
 dist_coeffs = np.array([[-6.07417419e-02,9.95447444e-02,-2.26448001e-04,1.22881804e-03,3.42134205e-03,1.45361886e-01,8.03248099e-02,2.11170107e-02,-3.80620047e-03,2.48350591e-05,-8.33565666e-04,2.97806723e-05]])
@@ -11,29 +12,31 @@ new_cam_mat, _ = cv2.getOptimalNewCameraMatrix(cam_mat, dist_coeffs, (1280, 960)
 map1, map2 = cv2.initUndistortRectifyMap(cam_mat, dist_coeffs, None, new_cam_mat, (1280, 960), cv2.CV_32FC1)
 
 async def stream_cameras(session: VuerSession, left_src=0, right_src=1):
-    pipeline_cam0 = (
-        "libcamerasrc camera-name=/base/axi/pcie@1000120000/rp1/i2c@88000/ov5647@36 ! "
-        "video/x-raw,format=BGR,width=1280,height=960,framerate=30/1 ! "
-        "videoconvert ! appsink drop=1 max-buffers=1"
+    cam_left = LibCameraCam(
+        log_dir=None,
+        resolution=LibCameraCam.ResolutionOption.R720P,
+        camera_name="/base/axi/pcie@1000120000/rp1/i2c@80000/ov5647@36",
+        framerate=45,
     )
-
-    pipeline_cam1 = (
-        "libcamerasrc camera-name=/base/axi/pcie@1000120000/rp1/i2c@80000/ov5647@36 ! "
-        "video/x-raw,format=BGR,width=1280,height=960,framerate=30/1 ! "
-        "videoconvert ! appsink drop=1 max-buffers=1"
+    cam_right = LibCameraCam(
+        log_dir=None,
+        resolution=LibCameraCam.ResolutionOption.R720P,
+        camera_name="/base/axi/pcie@1000120000/rp1/i2c@88000/ov5647@36",
+        framerate=45,
     )
-
-    cap1 = cv2.VideoCapture(pipeline_cam0, cv2.CAP_GSTREAMER)
-    cap2 = cv2.VideoCapture(pipeline_cam1, cv2.CAP_GSTREAMER)
+    
     while True:
-        ret_left, frame_left = cap1.read()
-        ret_right, frame_right = cap2.read()
-        if not ret_left or not ret_right:
+        img_left = cam_left.take_image()
+        img_right = cam_right.take_image()
+        if img_left is None or img_right is None:
             continue
+        
+        frame_left = img_left.get_array()
+        frame_right = img_right.get_array()
         frame_left_rgb = cv2.cvtColor(frame_left, cv2.COLOR_BGR2RGB)
         frame_right_rgb = cv2.cvtColor(frame_right, cv2.COLOR_BGR2RGB)
-        frame_left_rgb = cv2.undistort(frame_left_rgb, cam_mat, dist_coeffs, newCameraMatrix=new_cam_mat)
-        frame_right_rgb = cv2.undistort(frame_right_rgb, cam_mat, dist_coeffs, newCameraMatrix=new_cam_mat)
+        frame_left_rgb = cv2.remap(frame_left_rgb, map1, map2, cv2.INTER_LINEAR)
+        frame_right_rgb = cv2.remap(frame_right_rgb, map1, map2, cv2.INTER_LINEAR)
         # Add text labels for left/right cameras
         cv2.putText(frame_left_rgb, "Left Camera", (600, 30), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 4)
         cv2.putText(frame_right_rgb, "Right Camera", (600, 30), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 4)
