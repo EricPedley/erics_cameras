2e4461173db608154623e7c032a5dc737113dd63
Eric Pedley <ericpedley@gmail.com>
2025-08-19 13:34:14 -0700

    change undistort method

--- src/erics_cameras/scripts/vuer_videocap_vr_demo.py
+++ src/erics_cameras/scripts/vuer_videocap_vr_demo.py
@@ -8,8 +8,6 @@ from erics_cameras.libcamera_cam import LibCameraCam
 cam_mat = np.array([[266.61728276,0.,643.83126137],[0.,266.94450686,494.81811813],[0.,0.,1.,]])
 dist_coeffs = np.array([[-6.07417419e-02,9.95447444e-02,-2.26448001e-04,1.22881804e-03,3.42134205e-03,1.45361886e-01,8.03248099e-02,2.11170107e-02,-3.80620047e-03,2.48350591e-05,-8.33565666e-04,2.97806723e-05]])
 
-new_cam_mat, _ = cv2.getOptimalNewCameraMatrix(cam_mat, dist_coeffs, (1280, 960), 0, (1280, 960))
-map1, map2 = cv2.initUndistortRectifyMap(cam_mat, dist_coeffs, None, new_cam_mat, (1280, 960), cv2.CV_32FC1)
 
 async def stream_cameras(session: VuerSession, left_src=0, right_src=1):
     left_pipeline = "libcamerasrc camera-name=/base/axi/pcie@1000120000/rp1/i2c@80000/ov5647@36 exposure-time-mode=0 analogue-gain-mode=0 ae-enable=true awb-enable=true af-mode=manual ! video/x-raw,format=BGR,width=1280,height=720,framerate=30/1 ! videoconvert ! appsink drop=1 max-buffers=1"
@@ -24,8 +22,8 @@ async def stream_cameras(session: VuerSession, left_src=0, right_src=1):
             continue
         frame_left_rgb = cv2.cvtColor(frame_left, cv2.COLOR_BGR2RGB)
         frame_right_rgb = cv2.cvtColor(frame_right, cv2.COLOR_BGR2RGB)
-        frame_left_rgb = cv2.remap(frame_left_rgb, map1, map2, cv2.INTER_LINEAR)
-        frame_right_rgb = cv2.remap(frame_right_rgb, map1, map2, cv2.INTER_LINEAR)
+        frame_left_rgb = cv2.undistort(frame_left_rgb, cam_mat, dist_coeffs)
+        frame_right_rgb = cv2.undistort(frame_right_rgb, cam_mat, dist_coeffs)
         # Add text labels for left/right cameras
         cv2.putText(frame_left_rgb, "Left Camera", (600, 30), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 4)
         cv2.putText(frame_right_rgb, "Right Camera", (600, 30), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 4)
